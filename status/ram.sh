01bb3a29f9db75baee6fb869763bb83e48c7b687
pavelbezpravel <p.talashchenko@gmail.com>
2024-04-18 18:29:56 +0300

    feat: update colors

--- status/ram.sh
+++ status/ram.sh
@@ -4,11 +4,11 @@ show_ram() {
   index=$1
   icon=$(get_tmux_option "@catppuccin_ram_icon" "󰍛")
   color="$(get_tmux_option "@catppuccin_ram_color" "#{ram_bg_color}")"
-  text="$(get_tmux_option "@catppuccin_ram_text" "#{ram_percentage} ")"
+  text="$(get_tmux_option "@catppuccin_ram_text" " #{ram_percentage} ")"
 
-  tmux set-option -g @ram_low_bg_color "$thm_black4"     # background color when ram is low
-  tmux set-option -g @ram_medium_bg_color "$thm_magenta" # background color when ram is medium
-  tmux set-option -g @ram_high_bg_color "$thm_red"       # background color when ram is high
+  tmux set-option -g @ram_low_bg_color "$thm_green"     # background color when ram is low
+  tmux set-option -g @ram_medium_bg_color "$thm_orange" # background color when ram is medium
+  tmux set-option -g @ram_high_bg_color "$thm_red"      # background color when ram is high
 
   module=$(build_status_module "$index" "$icon" "$color" "$text")
 
